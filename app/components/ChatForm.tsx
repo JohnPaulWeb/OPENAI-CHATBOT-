d71b21734edb6242f6f2de68bbe4e87bd2a9eb0d
JohnPaulWeb <braganzajohnpaul@gmail.com>
2025-04-18 22:49:36 +0800

    AI CHATBOT

--- app/components/ChatForm.tsx
+++ app/components/ChatForm.tsx
@@ -30,15 +30,35 @@ export default function ChatForm({ className, ...props}: React.ComponentProps<"f
     }, [messages])
 
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-            e.preventDefault()
-            const form = e.currentTarget.form
-            if (form) {
-                handleSubmit(new FormEvent(form))
-            }
-        }
+    // const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    //     if (e.key === "Enter" && !e.shiftKey) {
+    //         e.preventDefault()
+    //         const form = e.currentTarget.form
+    //         if (form) {
+    //             handleSubmit(new FormEvent(form))
+    //         }
+    //     }
+    // }
+
+    const clearChat = () => {
+        localStorage.removeItem(`ai-chat-main-chat`)
+        window.location.reload()
     }
+
+    const header = (
+        <header className="m-auto flex max-w-96 flex-col gap-5 text-center">
+            <h1 className="text-2xl font-semibold leading-none tracking-tight">AI Chatbot</h1>
+            <p className="text-muted-foreground text-sm">
+                This is an AI chatbot app built in <span className="text-foreground">Nextjs</span>, the{" "}
+                <span className="text-foreground">Vercel AI SDK</span>, and
+                <span className="text-foreground">OPEN AI</span>
+            </p>
+
+            <p className="">Send A Message to start chatting wit the AI assistant </p>
+        </header>
+    )
+
+    
 
 
     return (
